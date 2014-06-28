b0021f060b2912889268013347b0f7eaf8873792
Ricardo Sánchez-Sáez <rsanchez.saez@gmail.com>
2014-06-28 13:40:40 +0100

    Replaced submodule dependencies by Cocoapods.
    Also:
    - Reorganized the project to a more standard way.
    - Moved Overshare example to an independent workspace. It now imports OvershareKit as the local Cocoapod itself.

--- OvershareKit/OSKTextViewAttachment.h
+++ OvershareKit/OSKTextViewAttachment.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 extern const CGFloat OSKTextViewAttachmentViewWidth_Phone;
 extern const CGFloat OSKTextViewAttachmentViewWidth_Pad;
