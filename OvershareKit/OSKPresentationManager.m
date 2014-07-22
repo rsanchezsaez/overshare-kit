dd6503d2b7223bd57b1eb0418bafdc8988dbd50c
Ricardo Sánchez-Sáez <rsanchez.saez@gmail.com>
2014-07-22 18:47:40 +0100

    Enable iOS 7 animation curve

--- OvershareKit/OSKPresentationManager.m
+++ OvershareKit/OSKPresentationManager.m
@@ -74,7 +74,7 @@ static NSInteger OSKTextViewFontSize_Pad = 20.0f;
 
 @end
 
-#define USE_UNDOCUMENTED_ANIMATION_CURVE 0
+#define USE_UNDOCUMENTED_ANIMATION_CURVE 1
 
 @implementation OSKPresentationManager
 
